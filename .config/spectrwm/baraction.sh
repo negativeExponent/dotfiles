8e54f4494c4440aab669e1e92f4dafcff72f25ab
negativeExponent <negativeExponent@users.noreply.github.com>
2020-08-05 14:07:37 +0000

    Increase font size and simplify spectrwm bar

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -14,7 +14,7 @@ hdd() {
 ## RAM
 mem() {
   mem=`free | awk '/Mem/ {printf "%dM/%dM\n", ($3+$5) / 1024.0, $2 / 1024.0 }'`
-  echo -e "$mem"
+  echo -e "MEM: $mem"
 }
 
 ## CPU
@@ -44,16 +44,16 @@ net() {
     eth=$(ip link | grep -m 1 -E '\b(en).*\b(state UP)' | awk '{print substr($2, 1, length($2)-1)}')
     wlan=$(ip link | grep -m 1 -E '\b(wl)' | awk '{print substr($2, 1, length($2)-1)}')
     if [ ! -z "$eth" ] ; then
-	rx1=`cat /sys/class/net/$eth/statistics/rx_bytes`
-	tx1=`cat /sys/class/net/$eth/statistics/tx_bytes`
-	sleep 0.5
-	rx2=`cat /sys/class/net/$eth/statistics/rx_bytes`
-	tx2=`cat /sys/class/net/$eth/statistics/tx_bytes`
-	rx=`expr $rx2 - $rx1`
-	tx=`expr $tx2 - $tx1`
-	downspeed=`expr $rx / 1024`
-	upspeed=`expr $tx / 1024`
-        echo -e "${downspeed}K / ${upspeed}K"
+        rx1=`cat /sys/class/net/$eth/statistics/rx_bytes`
+        tx1=`cat /sys/class/net/$eth/statistics/tx_bytes`
+        sleep 0.5
+        rx2=`cat /sys/class/net/$eth/statistics/rx_bytes`
+        tx2=`cat /sys/class/net/$eth/statistics/tx_bytes`
+        rx=`expr $rx2 - $rx1`
+        tx=`expr $tx2 - $tx1`
+        downspeed=`expr $rx / 1024`
+        upspeed=`expr $tx / 1024`
+        echo -e "NET: ${downspeed}K / ${upspeed}K"
     elif [ ! -z "$wlan" ] ; then
         echo -e "$wlan"
     else
@@ -61,8 +61,9 @@ net() {
     fi
 }
 
-SLEEP_SEC=3
+SLEEP_SEC=1
 while :; do
-    echo "+@fg=2; $(cpu)+@fg=0; | +@fg=3;  $(mem)+@fg=0; | +@fg=4; $(vol)+@fg=0; | +@fg=5;  $(net)+@fg=0; | "
+    #echo "+@fg=2; $(cpu)+@fg=0; | +@fg=3;  $(mem)+@fg=0; | +@fg=4; $(vol)+@fg=0; | +@fg=5;  $(net)+@fg=0; | "
+    echo "+@fg=3;$(cpu)+@fg=0; |  +@fg=4;$(mem)+@fg=0; |  +@fg=5;$(vol)+@fg=0; |  +@fg=6;$(net)+@fg=0; |"
 	sleep $SLEEP_SEC
 done
