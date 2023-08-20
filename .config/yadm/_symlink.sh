8d6aaf29a4b8697f29293a455439327ad8904612
negativeExponent <negativeExponent@users.noreply.github.com>
2023-08-20 19:42:51 +0800

    Change path from /mnt to /media

--- .config/yadm/_symlink.sh
+++ .config/yadm/_symlink.sh
@@ -14,22 +14,22 @@ create_symlinks() {
 	mkdir -p $HOME/.cache
 	mkdir -p $HOME/.config
 
-	link /mnt/data/yay $HOME/.cache/yay
+	link /media/data/yay $HOME/.cache/yay
 
 	# symlinks to HOME
-	link /mnt/data/Documents $HOME/Documents
-	link /mnt/data/Downloads $HOME/Downloads
-	link /mnt/data/Pictures $HOME/Pictures
+	link /media/data/Documents $HOME/Documents
+	link /media/data/Downloads $HOME/Downloads
+	link /media/data/Pictures $HOME/Pictures
 
-	link /mnt/data/Music $HOME/Music
+	link /media/data/Music $HOME/Music
 
-	link /mnt/data/myfiles/.mozilla $HOME/.mozilla
-	link /mnt/data/myfiles/ssh_key/.ssh $HOME/.ssh
+	link /media/data/myfiles/.mozilla $HOME/.mozilla
+	link /media/data/myfiles/ssh_key/.ssh $HOME/.ssh
 
 	# symlinks to .config
-	link /mnt/data/myfiles/BraveSoftware $HOME/.config/BraveSoftware
-	link /mnt/data/retroarch $HOME/.config/retroarch
-	link /mnt/data/myfiles/transmission-daemon $HOME/.config/transmission-daemon
+	link /media/data/myfiles/BraveSoftware $HOME/.config/BraveSoftware
+	link /media/data/retroarch $HOME/.config/retroarch
+	link /media/data/myfiles/transmission-daemon $HOME/.config/transmission-daemon
 }
 
 create_symlinks
